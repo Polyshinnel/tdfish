72be8853774c6541df6c3bd0c00a8b87bf4b1e59
Andrey <polyshinnel@gmail.com>
2021-07-20 21:24:35 +0300

    full homepage

--- assets/js/owl-settings.js
+++ assets/js/owl-settings.js
@@ -6,6 +6,37 @@ $(".banner-slider").owlCarousel({
     items: 1
 });
 
+$(".our-partners__slider").owlCarousel({
+    loop:true,
+    autoWidth:true,
+    dots: false,
+    items: 3,
+    margin: 12,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+            center: true,
+            margin: 40
+        },
+        740:{
+            items:2,
+        },
+        1000:{
+            items:3,
+            margin: 60
+        },
+        1290:{
+            items:3,
+            margin: 80
+        },
+        1420:{
+            items:4,
+            margin: 100
+        }
+    }
+});
+
 $('.banner-arrow-right').on('click', function() {
     $(this).parent().parent().find('.owl-next').trigger('click');
 });
