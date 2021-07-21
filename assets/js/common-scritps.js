d0165980d15cb8b5e544d64c4f6acdc1415df61c
Andrey <polyshinnel@gmail.com>
2021-07-21 15:43:32 +0300

    add recall form,correct text,add social link,add other link

--- assets/js/common-scritps.js
+++ assets/js/common-scritps.js
@@ -30,3 +30,47 @@ $(document).ready(function() {
         $('.header').addClass('header_border-bottom');
     }
 });
+
+$('.recall-form__base').on('click',function(){
+    $('.recall-form__overlay').fadeOut(400);
+});
+
+$('.recall-form__cross').on('click',function(){
+    $('.recall-form__overlay').fadeOut(400);
+    $('.recall-form__input-block input').val('');
+    $('.recall-form__region-select-input input').val('Регион:');
+});
+
+$('.recall-form__region-select-input').on('click',function(){
+    var state = $('.recall-form__region-select-block').css('display');
+    if(state == 'none'){
+        $('.input-angle').addClass('input-angle_rotate');
+    }else{
+        $('.input-angle').removeClass('input-angle_rotate');
+    }
+    $('.recall-form__region-select-block').slideToggle();
+});
+
+$('.recall-form__region-select-unit').on('click',function(){
+    var region = $(this).parent().find('p').html();
+    $(this).parent().parent().find('.recall-form__region-select-input input').val(region);
+    $('.recall-form__region-select-block').slideUp();
+    $('.input-angle').removeClass('input-angle_rotate');
+});
+
+$('.header__button-recall,.footer__btn-recall,.fish-sales__btn').on('click',function(){
+    $('.recall-form__overlay').fadeIn(400);
+});
+
+$('.header__mobile-navigation-recall-btn').on('click',function(){
+    $('.header__mobile-navigation').slideUp();
+    var menuImg = 'assets/img/menu-btn.svg';
+    $('.header__menu-btn img').attr('src', menuImg);
+    $('.recall-form__overlay').fadeIn(400);
+});
+
+$('.recall-form__btn').on('click',function(){
+    $('.recall-form__overlay').fadeOut(400);
+    $('.recall-form__input-block input').val('');
+    $('.recall-form__region-select-input input').val('Регион:');
+});
